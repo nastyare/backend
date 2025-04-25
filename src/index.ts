87a0327dc9d4be8a0dfb22a08bbaa824b0c35b04
Anastasia <anastasiya.reksius@mail.ru>
2025-04-26 00:03:16 +0700

    add routes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { connectDB } from "../src/config/db";
 import dotenv from "dotenv";
 import authRoutes from "../src/routes/authRoutes";
 import userRoutes from "../src/routes/userRoutes";
+import courseRoutes from "./routes/courseRoutes";
+import tagRoutes from "./routes/tagRoutes";
+import favoriteRoutes from "./routes/favoriteRoutes";
 
 dotenv.config();
 
@@ -14,6 +17,9 @@ connectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api", courseRoutes);
+app.use("/tags", tagRoutes);
+app.use("/favorites", favoriteRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
