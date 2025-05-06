66e01844bde8abdea0ae65d2b42648e73b25d108
Anastasia <anastasiya.reksius@mail.ru>
2025-05-06 11:58:42 +0700

    add lesson & comment routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import { connectDB } from "../src/config/db";
+import { connectDB } from "./config/db";
 import dotenv from "dotenv";
-import authRoutes from "../src/routes/authRoutes";
-import userRoutes from "../src/routes/userRoutes";
+import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
 import courseRoutes from "./routes/courseRoutes";
 import tagRoutes from "./routes/tagRoutes";
 import favoriteRoutes from "./routes/favoriteRoutes";
+import lessonRoutes from "./routes/lessonRoutes";
+import commentRoutes from "./routes/commentRoutes";
 
 dotenv.config();
 
@@ -17,9 +19,11 @@ connectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api", courseRoutes);
+app.use("/course", courseRoutes);
 app.use("/tags", tagRoutes);
 app.use("/favorites", favoriteRoutes);
+app.use("/lessons", lessonRoutes);
+app.use("/comments", commentRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
