3679248bbbcadf5f1bef3beba03b0796f821c7c1
Anastasia <anastasiya.reksius@mail.ru>
2025-05-06 12:02:32 +0700

    fix api

--- src/routes/courseRoutes.ts
+++ src/routes/courseRoutes.ts
@@ -16,7 +16,7 @@ const router = express.Router();
 router.get("/allcourses", getAllCourses);
 router.get("/:id", getCourseById);
 
-router.post("/courses", upload.single("image"), processImage, createCourse);
+router.post("/", upload.single("image"), processImage, createCourse);
 router.put("/courses/:id", processImage, updateCourse);
 router.delete("/courses/:id", deleteCourse);
 
